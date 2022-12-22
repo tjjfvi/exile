6c63bff70d6865e3c9977fec3c71d99da79c7b55
tjjfvi <t6@t6.fyi>
2022-12-22 07:40:57 -0500

    Checker.check wip

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kind2 to-hvm hvm-typechecker/checker.kind2 > program.hvm &&
+deno run -A ts-checker/main.ts >> program.hvm &&
+hvm r -f program.hvm
+
--- ts-checker/main.ts
+++ ts-checker/main.ts
@@ -1,9 +1,11 @@
 import { parse } from "./parser.ts";
 import { writeTerm } from "./write.ts";
 
-console.log(writeTerm(parse([
-  await Deno.readTextFile("Equal.xil"),
-  await Deno.readTextFile("Bool.xil"),
-  await Deno.readTextFile("Nat.xil"),
-  "one = Nat.succ Nat.zero; Nat.add one one",
-].join("\n\n"))));
+console.log(
+  "Program =\n" + writeTerm(parse([
+    await Deno.readTextFile("Equal.xil"),
+    await Deno.readTextFile("Bool.xil"),
+    // await Deno.readTextFile("Nat.xil"),
+    "Bool.and Bool.true",
+  ].join("\n\n"))),
+);
