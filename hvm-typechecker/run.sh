87e28b66ff744d0d6d9a7a4b190b62d4f413bdc4
FranchuFranchu <fff999abc999@gmail.com>
2022-12-21 21:14:35 -0300

    Modify run.sh script.

--- hvm-typechecker/run.sh
+++ hvm-typechecker/run.sh
@@ -1,1 +1,8 @@
-kind2 to-hvm checker.kind2 > checker.hvm && cat data.hvm >> checker.hvm && $HVM_PATH r -f checker.hvm
+#!/bin/bash
+
+if [ -n "$HVM_PATH" ]; then
+    kind2 to-hvm checker.kind2 > checker.hvm && cat data.hvm >> checker.hvm && $HVM_PATH r -f checker.hvm
+else
+    echo "Define the \$HVM_PATH variable please"
+fi
+
