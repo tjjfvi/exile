78603e770fcd785d00d4a9315aa6ea5460f85ad1
FranchuFranchu <fff999abc999@gmail.com>
2022-12-22 14:27:00 -0300

    Fix some bugs in typechecker that were introduced.
    Add commands to TS script.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 kind2 to-hvm hvm-typechecker/checker.kind2 > program.hvm &&
-deno run -A ts-checker/main.ts $@ >> program.hvm &&
+deno run -A ts-checker/main.ts $@ &&
 hvm r program.hvm
 
